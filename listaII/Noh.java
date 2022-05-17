71b8d298b59690cf9d091fddb4e6d41d805c4b7a
unknown <unipampa@U02L-2BVJ5V2.inst.unipampa.edu.br>
2022-05-16 21:38:38 -0300

    commit feita na aula do dia 16/05

---
+++ listaII/Noh.java
@@ -0,0 +1,17 @@
+package listaII;
+
+public class Noh {
+    private int info;
+    private Noh ant;
+    private Noh prox;
+
+    public Noh(int info){
+        this.info = info;
+        this.Noh = null;
+        this.Noh = null;
+    }
+
+    
+
+
+}
