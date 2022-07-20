584899deb4695b426613675edf875b12a9706dc0
Rafael S. de Lima <rafael.lima@startse.com>
2022-07-20 06:48:59 -0300

    implementado o novo método get e remove

--- Mapa/src/main/hashImplementacao/MapaHashVetor.java
+++ Mapa/src/main/hashImplementacao/MapaHashVetor.java
@@ -6,14 +6,21 @@ public class MapaHashVetor {
 	private Aluno[] vetor;
 	
 	public MapaHashVetor() {
-		this.vetor = new Aluno[20];
+		this.vetor = new Aluno[17];
 	}
 	private int hash(int chave) {
 		return chave % vetor.length;
 	}
 	public Aluno get(int chave) {
-		int hash = hash(chave);
-		return vetor[hash];
+		int sondagem = 0;
+		int hash;
+		while(sondagem < vetor.length) {
+			hash = (hash(chave)+sondagem)%vetor.length;
+			if(vetor[hash].getMatricula() == chave) {
+				return vetor[hash];
+			}
+		}
+		return null;
 	}
 	public void put(int chave, Aluno valor) {
 		int sondagem = 0;
@@ -28,9 +35,23 @@ public class MapaHashVetor {
 		}
 	}
 	public Aluno remove(int chave) {
-		int hash = hash(chave);
-		Aluno aluno = vetor[hash];
-		vetor[hash] = null;
-		return aluno;
+		int sondagem = 0;
+		int hash;
+		while(sondagem < vetor.length) {
+			hash = (hash(chave)+sondagem)%vetor.length;
+			if(vetor[hash].getMatricula() == chave) {
+				Aluno aluno = vetor[hash];
+				vetor[hash] = null;
+				return aluno;
+			}
+		}
+		return null;
+	}
+	
+	public void resize() {
+		
+	}
+	public void rehash() {
+		
 	}
 }
